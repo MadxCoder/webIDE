1c4567a03d6033918dd6afe5ac418e800324bec4
MadxCoder <43092873+MadxCoder@users.noreply.github.com>
2018-09-08 22:32:55 +0400

    Add files via upload

--- database.js
+++ database.js
@@ -8,15 +8,24 @@ var config = {
   messagingSenderId: "1051636511409"
 };
 firebase.initializeApp(config);
+
 var uploader = document.getElementById('uploader');
+var filename = document.getElementById('filename');
 uploader.addEventListener('click', function(e) {
-  var content = editor.getValue()+
-  '<style>'+editorcss.getValue()+'</style>'+
-  '<script>'+editorjs.getValue()+'</script>';
-  var file = new Blob([content],{type: "html;charset=utf-8"});
-  console.log(file);
-  var storageRef = firebase.storage().ref('html/1.html');
-  storageRef.put(file).then(function(snapshot) {
-  console.log('code uploaded!');
-});
+  var name = filename.value;
+  if (name !== "") {
+    var content = editor.getValue()+
+    '<style>'+editorcss.getValue()+'</style>'+
+    '<script>'+editorjs.getValue()+'</script>';
+    var file = new Blob([content],{type: "html;charset=utf-8"});
+    console.log(file);
+    var storageRef = firebase.storage().ref('html/'+name);
+    storageRef.put(file).then(function(snapshot) {
+      console.log('code uploaded!');
+    });
+  }
+  else {
+    filename.classList.add("inputError");
+    alert('please insert a file name');
+  }
 });
